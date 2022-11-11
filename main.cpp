844267c0c2c8a38ca8ae4c6ca0650dba0b17fe83
ilyassx1234 <117658214+ilyassx1234@users.noreply.github.com>
2022-11-11 19:45:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 //Qui vanno definite le variabili
     float a=0,b=0,c=0;
-
+ 
     cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
